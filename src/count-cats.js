b524f0e60c3df1842c37f588701e8faa0f35160c
RikoGit <e.heregoes@gmail.com>
2022-10-17 19:48:39 +0300

    feat: implement Count cats!

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,7 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+
+const countCats = (matrix) => {
+  if (!Array.isArray(matrix)) {
+    throw new Error("The argument is not an array!");
+  }
+
+  return matrix.reduce((count, row) => {
+    count += row.filter((cell) => cell === "^^").length;
+
+    return count;
+  }, 0);
+};
 
 module.exports = {
-  countCats
+  countCats,
 };
