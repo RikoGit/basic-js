e0960d17bac829c8039113a09f61d793aa3391e7
RikoGit <e.heregoes@gmail.com>
2022-10-17 21:13:22 +0300

    feat: implement Minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,11 +21,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+
+const minesweeper = (matrix) => {
+  const transformedMatrix = matrix.map((row) => row.map((cell) => 0));
+
+  matrix.forEach((row, i) => {
+    row.forEach((cell, y) => {
+      if (cell === false) {
+        return;
+      }
+
+      if (cell === true) {
+        if (i > 0) {
+          transformedMatrix[i - 1][y] += 1;
+        }
+        if (i < matrix.length) {
+          transformedMatrix[i + 1][y] += 1;
+        }
+
+        if (y > 0) {
+          transformedMatrix[i][y - 1] += 1;
+          if (i > 0) {
+            transformedMatrix[i - 1][y - 1] += 1;
+          }
+
+          if (i < matrix.length) {
+            transformedMatrix[i + 1][y - 1] += 1;
+          }
+        }
+
+        if (y < row.length - 1) {
+          transformedMatrix[i][y + 1] += 1;
+          if (i > 0) {
+            transformedMatrix[i - 1][y + 1] += 1;
+          }
+          if (i < matrix.length) {
+            transformedMatrix[i + 1][y + 1] += 1;
+          }
+        }
+      }
+    });
+  });
+
+  return transformedMatrix;
+};
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
