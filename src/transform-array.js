2376eadfc1b6a9e1575f5b38358a0e83ce807c68
RikoGit <e.heregoes@gmail.com>
2022-10-17 20:46:15 +0300

    feat: implement Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,82 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+
+const transform = (arr) => {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+
+  const flags = [
+    "--discard-prev",
+    "--discard-next",
+    "--double-prev",
+    "--double-next",
+  ];
+
+  let flag = false;
+  let isItemRemoved = false;
+
+  return arr.reduce((transformedArray, item) => {
+    if (flags.includes(item)) {
+      flag = item;
+
+      return transformedArray;
+    }
+    if (!flag) {
+      transformedArray.push(item);
+
+      return transformedArray;
+    }
+    if (flag === "--discard-prev") {
+      if (isItemRemoved) {
+        transformedArray.push(item);
+        flag = false;
+        isItemRemoved = false;
+
+        return transformedArray;
+      }
+      if (transformedArray.length !== 0) {
+        transformedArray.pop();
+      }
+      transformedArray.push(item);
+    }
+    if (flag === "--double-prev") {
+      if (isItemRemoved) {
+        transformedArray.push(item);
+        flag = false;
+        isItemRemoved = false;
+
+        return transformedArray;
+      }
+      if (transformedArray.length !== 0) {
+        const last = transformedArray[transformedArray.length - 1];
+        transformedArray.push(last);
+      }
+      transformedArray.push(item);
+    }
+    if (flag === "--discard-next") {
+      isItemRemoved = true;
+    }
+    if (flag === "--double-next") {
+      transformedArray.push(item, item);
+    }
+    flag = false;
+
+    return transformedArray;
+  }, []);
+};
 
 module.exports = {
-  transform
+  transform,
 };
